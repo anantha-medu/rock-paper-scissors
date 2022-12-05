cc99702ba58b832fc4cc2527e68e123288a3ee3c
Anantha Medu <ananthamedu18@gmail.com>
2022-12-05 16:11:26 +0530

    Add functionally  working page of the game

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -155,8 +155,6 @@ const compScoreDisplay = document.querySelector('#comp-score');
 let playerScore = 0;
 let compScore = 0;
 
-playerScoreDisplay.textContent = playerScore;
-compScoreDisplay.textContent = compScore;
 
 // to display winner and loser color after each round
 
@@ -165,6 +163,7 @@ let showResultColor = (winner, loser) =>
     winner.parentElement.classList.toggle('winner');
     loser.parentElement.classList.toggle('loser');
 }
+
 
 
 
@@ -199,7 +198,7 @@ function getComputerChoice()
 
 const playRoundButton = document.getElementById('play-round-btn');
 
-playRoundButton.addEventListener("click", playRound);
+playRoundButton.addEventListener("click",game);
 
 function getPlayerChoice()
 {
@@ -220,36 +219,37 @@ function getPlayerChoice()
 
 //Function to play a round of the game
 
+const resultDisplay = document.querySelector('#result-display');
+
 function playRound()
 {
     const computerChoice = getComputerChoice();
     const playerSelection = getPlayerChoice();
-    let result;
     // console.log(computerChoice);
     // console.log(playerChoice);
-    const resultDisplay = document.querySelector('#result-display');
+    resultDisplay.style.display = 'block';
 
     if(computerChoice === playerSelection)
     {
         // console.log("It's a draw");
-        resultDisplay.innerHTML = "It's a draw";
-        return result = "Draw";
+        resultDisplay.textContent = "It's a draw";
+        return "Draw";
     }
 
     else if((computerChoice==="rock" && playerSelection==="scissors") || (computerChoice==="paper" && playerSelection==="rock") ||  (computerChoice==="scissors" && playerSelection==="paper"))
     {
         // console.log("You lose the round! Computer wins!");
-        resultDisplay.innerHTML = "You lose the round! Computer wins!";
+        resultDisplay.textContent = "You lose the round! Computer wins!";
         showResultColor(compChoiceDisplay, playerChoice);
-        return result = "Lose";
+        return "Lose";
     }
 
     else
     {
         // console.log("You win the round!");
-        resultDisplay.innerHTML = "You win the round!";
+        resultDisplay.textContent = "You win the round!";
         showResultColor(playerChoice, compChoiceDisplay);
-        return result = "Win";
+        return "Win";
     }
 }
 
@@ -266,4 +266,69 @@ function updateScore(text)
     {
         compScore += 1;
     }
+
+        playerScoreDisplay.textContent = playerScore;
+        compScoreDisplay.textContent = compScore;
 }
+
+// to display final result
+
+const finalResultText = document.getElementById('final-result');
+
+function displayFinalResult()
+{
+    document.getElementById('play-round-btn').style.display = 'none';
+    resetButton.textContent = "PLAY AGAIN"
+    finalResultText.style.display = 'block';
+    if(playerScore > compScore)
+    {
+        finalResultText.textContent = "Congratulations! You won the game!";
+    }
+    else
+    {
+        finalResultText.textContent = "Sorry! You lost the game. Better luck next time!";
+    }
+
+    finalResultText
+}
+
+
+// final game function to connect all functions and streamline them
+function game()
+{
+    result = playRound();
+    updateScore(result);
+    if(playerScore===5 || compScore === 5)
+    {
+        displayFinalResult();
+    }
+
+}
+
+// reset button when clicked
+
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener("click", resetOnClick);
+
+function resetOnClick()
+{
+    playerScore = 0;
+    compScore = 0;
+    
+    playerScoreDisplay.textContent = playerScore;
+    compScoreDisplay.textContent = compScore;
+
+    document.getElementById('play-round-btn').style.display = 'block';
+    playerChoice.parentElement.classList.remove('winner', 'loser');
+    compChoiceDisplay.parentElement.classList.remove('winner', 'loser');
+
+    resetButton.textContent = "RESTART"
+
+    playerChoice.src = 'img/question-mark.png';
+    compChoiceDisplay.src = 'img/question-mark.png';
+
+    resultDisplay.style.display = 'none';
+    finalResultText.style.display = 'none';
+
+}
+
